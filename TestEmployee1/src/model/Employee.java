c4f073e86367fa1aab59044d8859abcd3882da37
elilueders <70550355+elilueders@users.noreply.github.com>
2020-09-10 20:15:26 -0500

    initial commit

---
+++ TestEmployee1/src/model/Employee.java
@@ -0,0 +1,43 @@
+package model;
+
+public class Employee {
+
+	private String name;
+	private double monthlySalary;
+	private int age;
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMonthlySalary() {
+		return monthlySalary;
+	}
+
+	public void setMonthlySalary(double monthlySalary) {
+		this.monthlySalary = monthlySalary;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
